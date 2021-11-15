830e5cd9156d6bce55b7214d57ac437ede32cb59
ruslana <ruslana.voznyuk@gmail.com>
2021-11-15 19:51:07 +0200

    create package tracks (module, controller, service)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,9 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { TrackModule } from "./track/track.module"
 
 
 @Module ({
-	controllers: [AppController],
-	providers: [AppService]
+	imports: [TrackModule]
 })
 
 
