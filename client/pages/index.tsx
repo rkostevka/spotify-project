d76c620938270313485928ff437f42223eee2e0c
ruslana <ruslana.voznyuk@gmail.com>
2021-11-17 14:44:44 +0200

    Client: main page with Navbar Material UI

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,27 @@
+import { Button } from '@mui/material';
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Index = () => {
     return (
-        <div>
-                Main Page
-        </div>
+        <>
+            <Navbar/>
+            <div className="center">
+                <h1>Welcome!</h1>
+                <h3>Here are the most awesome tracks!</h3>
+            </div>
+            <style jsx>
+                {`
+                  .center {
+                    margin-top: 150px;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+                  }
+                `}
+            </style>
+        </>
     );
 };
 
