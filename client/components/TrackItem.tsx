7446eaf3051c685a219ff5c4225eb595d31d6f46
ruslana <ruslana.voznyuk@gmail.com>
2021-11-17 17:22:26 +0200

    Client: Page with track list (add scss file)

--- client/components/TrackItem.tsx
+++ client/components/TrackItem.tsx
@@ -1,5 +1,8 @@
+import { Card, Grid, IconButton } from '@mui/material';
 import React from 'react';
 import { ITrack } from '../types/tracks';
+import styles from '../styles/TrackItem.module.scss';
+import {Delete, Pause, PlayArrow } from '@mui/icons-material';
 
 interface TrackItemProps {
     track: ITrack;
@@ -8,9 +11,26 @@ interface TrackItemProps {
 
 const TrackItem: React.FC<TrackItemProps> = ({track, active = false}) => {
     return (
-        <div>
-            {track.name}
-        </div>
+        <Card className={styles.track}>
+            <IconButton>
+                {active
+                    ? <Pause/>
+                    : <PlayArrow/>
+                }
+            </IconButton>
+            <img width={70} height={70} src={track.picture} />
+            <Grid container direction="column" className={styles.trackTitle}>
+                <div>{track.name}</div>
+                <div className={styles.artist}>{track.artist}</div>
+            </Grid>
+            <div className={styles.rightBlock}>
+                {active && <div>02:42 / 03:22</div>}
+                <IconButton>
+                    <Delete />
+                </IconButton>
+            </div>
+
+        </Card>
     );
 };
 
