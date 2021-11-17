8dfb9508354d8d11893056810f582d8c2baf15f5
ruslana <ruslana.voznyuk@gmail.com>
2021-11-17 10:45:57 +0200

    Server: file service(audio and pictures in several packages)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,18 @@ import { Module } from "@nestjs/common";
 import { TrackModule } from "./track/track.module";
 import { MongooseModule } from '@nestjs/mongoose';
 import { FileModule } from "./file/file.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from "path";
 
 
 @Module ({
 	imports: [
 		TrackModule,
 		MongooseModule.forRoot('mongodb+srv://root:root@cluster0.wk9im.mongodb.net/Spotify-music?retryWrites=true&w=majority'),
-		FileModule
+		FileModule,
+		ServeStaticModule.forRoot({
+			rootPath: path.resolve(__dirname, 'static'),
+		}),
 	]
 })
 
