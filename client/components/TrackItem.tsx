e742f957e700ecd490ce5e10ba5d59a122bddbe4
ruslana <ruslana.voznyuk@gmail.com>
2021-11-17 19:15:07 +0200

    Client: Track List, Track item page

--- client/components/TrackItem.tsx
+++ client/components/TrackItem.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { ITrack } from '../types/tracks';
 import styles from '../styles/TrackItem.module.scss';
 import {Delete, Pause, PlayArrow } from '@mui/icons-material';
+import { useRouter } from 'next/router';
 
 interface TrackItemProps {
     track: ITrack;
@@ -10,20 +11,21 @@ interface TrackItemProps {
 }
 
 const TrackItem: React.FC<TrackItemProps> = ({track, active = false}) => {
+    const router = useRouter();
     return (
-        <Card className={styles.track}>
-            <IconButton>
+        <Card className={styles.track} onClick={() => router.push('/tracks/' + track._id)}>
+            <IconButton onClick={(e) => e.stopPropagation()}>
                 {active
                     ? <Pause/>
                     : <PlayArrow/>
                 }
             </IconButton>
-            <img width={70} height={70} src={track.picture} />
+            <img width={70} height={70} src={track.picture} onClick={(e) => e.stopPropagation()} />
             <Grid container direction="column" className={styles.trackTitle}>
                 <div>{track.name}</div>
                 <div className={styles.artist}>{track.artist}</div>
             </Grid>
-            <div className={styles.rightBlock}>
+            <div className={styles.rightBlock} onClick={(e) => e.stopPropagation()}>
                 {active && <div>02:42 / 03:22</div>}
                 <IconButton>
                     <Delete />
---
+++ client/pages/tracks/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TrackPage: React.FC = () => {
+    return (
+        <div>
+            TrackPage
+        </div>
+    );
+};
+
+    export default TrackPage;
