dc5ae082af10b45835e200c3e35f9cd3ed611098
ruslana <ruslana.voznyuk@gmail.com>
2021-11-17 11:03:39 +0200

    Server: getAll with limit and offset, add listens

--- server/src/track/track.controller.ts
+++ server/src/track/track.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Delete, Get, Param, Post, UploadedFiles, UseInterceptors } from "@nestjs/common"
+import {Body, Controller, Delete, Get, Param, Post, Query, UploadedFiles, UseInterceptors } from "@nestjs/common"
 import { FileFieldsInterceptor } from "@nestjs/platform-express";
 
 import { ObjectId } from "mongoose";
@@ -21,10 +21,10 @@ export class TrackController {
         return this.trackService.create(dto, picture[0], audio[0]);
     }
 
-
     @Get()
-    getAll() {
-        return this.trackService.getAll();
+    getAll(@Query('count') count:number,
+           @Query('offset') offset: number) {
+        return this.trackService.getAll(count, offset);
     }
 
     @Get(':id')
@@ -40,6 +40,11 @@ export class TrackController {
     @Post('/comment')
     addComment(@Body() dto: CreateCommentDto) {
         return this.trackService.addComment(dto);
+    }
+
+    @Post('/listen/:id')
+    listen(@Param('id') id: ObjectId) {
+        return this.trackService.listen(id);
     }
 
 }
--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@nestjs/common"
 import { InjectModel } from "@nestjs/mongoose";
-import { Model, ObjectId } from "mongoose";
-import { Track, trackDocument } from "./schemas/track.schema";
-import { Comment, commentDocument } from "./schemas/comment.schema";
-import { CreateTrackDto } from "./dto/create-track.dto";
-import { CreateCommentDto } from "./dto/create-comment.dto";
-import { FileService, FileType } from "src/file/file.service";
+import { Model, ObjectId, Schema} from "mongoose";
+import {Track, trackDocument} from "./schemas/track.schema";
+import {Comment, commentDocument} from "./schemas/comment.schema";
+import {CreateTrackDto} from "./dto/create-track.dto";
+import {CreateCommentDto} from "./dto/create-comment.dto";
+import {FileService, FileType} from "src/file/file.service";
 
 @Injectable()
 export class TrackService {
@@ -20,8 +20,8 @@ export class TrackService {
         const track = await this.trackModel.create({...dto, listens: 0, picture: picturePath, audio: audioPath});
         return track;
     }
-    async getAll(): Promise<Track[]> {
-        const tracks = await this.trackModel.find();
+    async getAll(count = 10, offset = 0): Promise<Track[]> {
+        const tracks = await this.trackModel.find().skip(Number(offset)).limit(Number(count));
         return tracks;
     }
 
@@ -40,5 +40,11 @@ export class TrackService {
         track.comments.push(comment._id);
         await track.save();
         return comment;
+    }
+
+    async listen(id: ObjectId) {
+        const track = await this.trackModel.findById(id);
+        track.listens += 1;
+        track.save();
     }
 }
