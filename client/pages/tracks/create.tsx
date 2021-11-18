f879d4bee9554ae67b7ae55252a82c1020acdf0d
ruslana <ruslana.voznyuk@gmail.com>
2021-11-18 14:34:53 +0200

    Client: Component for downloading files

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,11 +1,14 @@
 import {Button, Grid, TextField } from '@mui/material';
 import React, { useState } from 'react';
+import FileUpload from '../../components/FileUpload';
 import StepWrapper from '../../components/StepWrapper';
 import MainLayout from '../../layouts/MainLayout';
 import styles from '../../styles/StepWrapper.module.scss';
 
 const Create = () => {
     const [activeStep, setActiveStep] = useState(0);
+    const [picture, setPicture] =useState(null);
+    const [audio, setAudio] = useState(null);
     const next = () => {
         if(activeStep !== 2) {
             setActiveStep(prev => prev + 1);
@@ -31,8 +34,16 @@ const Create = () => {
                         />
                     </Grid>
                 }
-                {activeStep == 1 && <div>Step 2</div>}
-                {activeStep == 2 && <div>Step 3</div>}
+                {activeStep == 1 &&
+                    <FileUpload setFile={setPicture} accept={'image/*'}>
+                        <Button>Download cover of track</Button>
+                    </FileUpload>
+                }
+                {activeStep == 2 &&
+                    <FileUpload setFile={setAudio} accept={'audio/*'}>
+                        <Button>Download track</Button>
+                    </FileUpload>
+                }
             </StepWrapper>
             <Grid container justifyContent="space-between">
                 <Button onClick={back}>Back</Button>
